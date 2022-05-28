2d979df8ffe7ecf99535458d816099c361000d1d
mlabenski <mlabenski123@gmail.com>
2022-05-28 17:41:16 -0400

    gotta be a smarter way to redirect based on permissions

---
+++ pages/dashboard.vue
@@ -0,0 +1,310 @@
+<template>
+  <section id="hero">
+      <v-row align="center" justify="center" style="height: 650px">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="6" xl="8">
+              <h1 class="display-2 font-weight-bold mb-4">wr \ APP / er</h1>
+              <h1 class="font-weight-light">
+                Quick solution for maintaining a<br />
+                shopping cart without web hosting <br />
+                expenses, maintenance, or vulnerabilties!
+              </h1>
+              <v-btn
+                rounded
+                outlined
+                large
+                dark
+                @click="nav()"
+                class="mt-5"
+              >
+                Generate One
+                <v-icon class="ml-2">mdi-arrow-down</v-icon>
+              </v-btn>
+              <div class="video d-flex align-center py-4">
+                <a @click.stop="dialog = true" class="playBut">
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+                    x="0px"
+                    y="0px"
+                    width="60px"
+                    height="60px"
+                    viewBox="0 0 213.7 213.7"
+                    enable-background="new 0 0 213.7 213.7"
+                    xml:space="preserve"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                    xsi:schemaLocation="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/ ">
+                    <polygon
+                      class="triangle"
+                      id="XMLID_18_"
+                      fill="none"
+                      stroke-width="7"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      points="73.5,62.5 148.5,105.8 73.5,149.1 "
+                    />
+
+                    <circle
+                      class="circle"
+                      id="XMLID_17_"
+                      fill="none"
+                      stroke-width="7"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      cx="106.8"
+                      cy="106.8"
+                      r="103.3"
+                    />
+                  </svg>
+                </a>
+                <p class="subheading ml-2 mb-0">Welcome back loyal merchant</p>
+              </div>
+            </v-col>
+            <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <div class="svg-border-waves text-white">
+        <v-img src="@/assets/img/borderWaves.svg" />
+      </div>
+    <v-container fluid id="features" class="mt-2">
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="space-around">
+            <!-- <v-col cols="12" class="text-center">
+              <h1 class="font-weight-light display-2">Title</h1>
+              <h1 class="font-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h1>
+            </v-col> -->
+            <v-col
+              cols="12"
+              sm="4"
+              class="text-center"
+              v-for="(feature, i) in features"
+              :key="i"
+            >
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  class="card"
+                  shaped
+                  :elevation="hover ? 10 : 4"
+                  :class="{ up: hover }"
+                >
+                  <v-img
+                    :src="feature.img"
+                    max-width="100px"
+                    class="d-block ml-auto mr-auto"
+                    :class="{ 'zoom-efect': hover }"
+                  ></v-img>
+                  <h1 class="font-weight-regular">{{ feature.title }}</h1>
+                  <h4 class="font-weight-regular subtitle-1">
+                    {{ feature.text }}
+                  </h4>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="svg-border-waves">
+      <img src="~@/assets/img/wave2.svg" />
+      <div v-if="isLoggedIn" v:on:click="triggerNetlifyIdentityAction('logout')" class="button--grey">Logout</div>
+      <div v-else v:on:click="triggerNetlifyIdentityAction('login')" class="button--grey">Login</div>
+      <nuxt-link to="/product-entry" class="button--green">
+        Protected Page
+      </nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+import netlifyIdentity from 'netlify-identity-widget';
+import { mapState, mapActions } from "vuex";
+let currentUser 
+if (process.browser) {
+  netlifyIdentity.init({
+    APIUrl: 'https://usewrapper.com/.netlify/identity'
+  })
+  currentUser = netlifyIdentity.currentUser();
+}
+
+export default {
+  name: 'IndexPage',
+  auth: false,
+  data() {
+    return {
+      // eslint-disable-next-line object-shorthand
+      currentUser: currentUser,
+      dialog: false,
+      features: [
+        {
+          img: require("@/assets/img/icon2.png"),
+          title: "Step 1",
+          text: "Import the products you wish to sell online.",
+        },
+        {
+          img: require("@/assets/img/icon1.png"),
+          title: "Step 2",
+          text: "Choose between retail, restaurant, or digital shop templates.",
+        },
+        {
+          img: require("@/assets/img/icon3.png"),
+          title: "Step 3",
+          text: "Provide your shop URL to customers and wait for transactions!.",
+        },
+      ],
+    };
+  },
+  computed: mapState({
+    isLoggedIn: state => state.currentUser
+  }),
+  methods: {
+    ...mapActions({
+      setUser: 'handleUpdateUser'
+    }),
+    triggerNetlifyIdentityAction(action) {
+      if(action === "login" || action === "signup") {
+        netlifyIdentity.open(action);
+        netlifyIdentity.on(action, user => {
+          this.setUser(user);
+          netlifyIdentity.close();
+        });
+      } else if (action === "logout") {
+        this.setUser(null);
+        netlifyIdentity.logout();
+        this.$router.push('/');
+      }
+    },
+    nav() {
+      this.$router.push('/step-one')
+    },
+    ready(event) {
+      this.player = event.target;
+    },
+    playing(event) {
+      // The player is playing a video.
+    },
+    change() {
+      // when you change the value, the player will also change.
+      // If you would like to change `playerVars`, please change it before you change `videoId`.
+      // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
+      // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
+      this.videoId = "another video id";
+    },
+    stop() {
+      this.player.stopVideo();
+    },
+    pause() {
+      this.player.pauseVideo();
+    },
+  }
+};
+</script>
+
+<style lang="scss">
+.circle {
+  stroke: white;
+  stroke-dasharray: 650;
+  stroke-dashoffset: 650;
+  -webkit-transition: all 0.5s ease-in-out;
+  opacity: 0.3;
+}
+.playBut {
+  /*  border: 1px solid red;*/
+  display: inline-block;
+  -webkit-transition: all 0.5s ease;
+  .triangle {
+    -webkit-transition: all 0.7s ease-in-out;
+    stroke-dasharray: 240;
+    stroke-dashoffset: 480;
+    stroke: white;
+    transform: translateY(0);
+  }
+  &:hover {
+    .triangle {
+      stroke-dashoffset: 0;
+      opacity: 1;
+      stroke: white;
+      animation: nudge 0.7s ease-in-out;
+      @keyframes nudge {
+        0% {
+          transform: translateX(0);
+        }
+        30% {
+          transform: translateX(-5px);
+        }
+        50% {
+          transform: translateX(5px);
+        }
+        70% {
+          transform: translateX(-2px);
+        }
+        100% {
+          transform: translateX(0);
+        }
+      }
+    }
+    .circle {
+      stroke-dashoffset: 0;
+      opacity: 1;
+    }
+  }
+}
+</style>
+
+<style>
+.btn-play {
+  transition: 0.2s;
+}
+.svg-border-waves .v-image {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  overflow: hidden;
+}
+#hero {
+  z-index: 0;
+}
+.svg-border-waves img {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin-bottom: -2px;
+  z-index: -1;
+}
+.card {
+  min-height: 300px;
+  padding: 10px;
+  transition: 0.5s ease-out;
+}
+.card .v-image {
+  margin-bottom: 15px;
+  transition: 0.75s;
+}
+.card h1 {
+  margin-bottom: 10px;
+}
+.zoom-efect {
+  transform: scale(1.1);
+}
+.up {
+  transform: translateY(-20px);
+  transition: 0.5s ease-out;
+}
+</style>
+
+<style>
+section {
+  position: relative;
+}
+</style>
