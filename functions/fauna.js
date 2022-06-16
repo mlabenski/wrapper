6053f70486cbb92dc5d790f2c63616551171c7ed
mlabenski <mlabenski123@gmail.com>
2022-06-16 00:00:41 -0400

    almost thursday, still netlify commits

--- functions/fauna.js
+++ functions/fauna.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require('node-fetch');
 
 exports.faunaFetch = async ({ query, variables }) => {
   return await fetch('https://graphql.fauna.com/graphql', {
