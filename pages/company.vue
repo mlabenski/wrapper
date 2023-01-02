cc11cd0d2a83e0bb9e82ba0461f75c8b5b8caa8b
Mitchell LaBenski <mlabenski123@gmail.com>
2023-01-02 18:17:30 -0500

    idk

--- pages/company.vue
+++ pages/company.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-parallax class="my-5" src="@/assets/img/material-parallex.jpg">
+    <v-parallax class="my-5" src="path/to/image.jpg">
       <v-row class="text-center white--text fill-height">
         <v-col cols="12">
           <h1 class="display-1 font-weight-bold">Wrapper</h1>
@@ -10,7 +10,7 @@
     </v-parallax>
     <v-row>
       <v-col cols="12" md="6">
-        <v-card class="mx-auto" max-width="700" style="width: 100%; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.12);">
+        <v-card class="mx-auto" max-width="700" style="width: 100%; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.12);" @click="goToUseWrapperPage" :to="{ name: 'use-wrapper' }" :elevation="hovering1 ? 12 : 2" @mouseover="hovering1 = true" @mouseleave="hovering1 = false">
           <v-card-title class="text-center">Use Wrapper</v-card-title>
           <v-card-subtitle class="text-center">E-commerce</v-card-subtitle>
           <v-card-text>
@@ -19,12 +19,13 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="6">
-        <v-card class="mx-auto" max-width="700" style="width: 100%; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.12);">
+        <v-card class="mx-auto" max-width="700" style="width: 100%; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.12);" @click="goToWrapperForWooPage" :to="{ name: 'wrapper-for-woo' }" :elevation="hovering2 ? 12 : 2" @mouseover="hovering2 = true" @mouseleave="hovering2 = false">
           <v-card-title class="text-center">Wrapper for Woo</v-card-title>
           <v-card-subtitle class="text-center">Financial Tech</v-card-subtitle>
           <v-card-text>
-            <p>Wrapper for Woo is an extension to the popular WooCommerce plugin for WordPress. It adds an additional payment method to your online store, allowing you to accept payments from additional payment processors. With Wrapper for Woo, you can expand your payment options and provide an even better shopping experience for your customers. You can download Wrapper for Woo at https://wordpress.org/plugins/wrapper-checkout-for-cardpointe-and-hpp/. Start accepting more payments today with Wrapper for Woo!</p>
-          </v-card-text>
+            <p>Wrapper for Woo is an extension to the popular WooCommerce plugin for WordPress. It adds an additional payment method to your online store, allowing you to accept payments from additional payment processors. With Wrapper for Woo, you can expand your payment options and provide an even better shopping experience for your customers.
+            </p>
+            </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -32,8 +33,26 @@
 </template>
 <script>
   export default {
-    name: 'Company'
+    name: 'Company',
+  
+  data() {
+    return {
+      hovering1: false,
+      hovering2: false
+    }
+  },
+  methods: {
+    goToUseWrapperPage() {
+      this.$router.push({ name: 'use-wrapper' })
+    },
+    goToWrapperForWooPage() {
+      window.location.href = "https://wordpress.org/plugins/wrapper-checkout-for-cardpointe-and-hpp/"
+    },
+    nav() {
+      this.$router.push('/step-one')
+    },
   }
+}
 </script>
   
 <style>
