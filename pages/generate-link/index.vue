c59b0df49de57f60f05c853237ee991b30831f92
geebski <mlabenski123@gmail.com>
2023-05-28 23:26:29 +0000

    updates to copy clipboard

--- pages/generate-link/index.vue
+++ pages/generate-link/index.vue
@@ -44,7 +44,7 @@
               <v-card-text class="text-body-1">
                 <p class="mb-4">This chat expects a sentence describing a payment link you wish to send your customer. Our system will translate the input to a payment URL, the only step left is to message the link to them.</p>
                 <v-divider></v-divider>
-                <p class="mt-4">While the system is programmed to recognize valid input, an example is: "The website name is DaveNBusters the customer name is John Doe and his customer id is 39991. He is buying the item two hundred tickets package, which costs in total $200, the quantity of this order is 1. He is also buying two skittles family sized for $4.99 each."</p>
+                <p class="mt-4">While the system is programmed to recognize valid input, an example is: "The HPP URL is DaveNBusters the customer name is John Doe and his customer id is 39991. He is buying the item two hundred tickets package, which costs in total $200, the quantity of this order is 1. He is also buying two skittles family sized for $4.99 each."</p>
                 <p>Expected answer: <a href="https://cpswoo.securepayments.cardpointe.com/pay?total=209.98&customerId=39991&billFName=John&billLName=Doe&details=200%20Tickets%20Package%7C1%7C200%3C%3ESkittles%20Family%20Sized%7C2%7C4.99">Example Payment Link</a></p>
               </v-card-text>
               <v-card-actions>
@@ -69,7 +69,21 @@
       :color="isOutputHovered ? 'blue' : ''"
       @mouseover="isOutputHovered = true"
       @mouseleave="isOutputHovered = false"
+      @click="copiedToClipboard = true"
     ></v-textarea>
+    <v-snackbar
+    v-model="copiedToClipboard"
+    timeout="3000"
+  >
+    Copied to clipboard
+    <v-btn
+      color="blue"
+      text
+      @click="copiedToClipboard = false"
+    >
+      Close
+    </v-btn>
+  </v-snackbar>
   </v-col>
   </v-row>
 </template>
@@ -85,13 +99,14 @@ export default {
       outputText: '',
       dialog: false,
       suggestions: [
-        'the website title is',
+        'the hpp url is is',
         'the customers name is',
         'the item name is',
         'the price of this item is',
         'the quantity of this item is'
       ],
-      isOutputHovered: false
+      isOutputHovered: false,
+      copiedToClipboard: false,
     }
   },
   mounted() {
@@ -118,7 +133,7 @@ export default {
       this.typedText += ` ${suggestion} `;
     },
     tryPrompt() {
-      this.typedText = 'The website name is DaveNBusters the customer name is John Doe and his customer id is 39991. He is buying the item two hundred tickets package, which costs in total $200, the quantity of this order is 1. He is also buying two skittles family sized for $4.99 each.';
+      this.typedText = 'The hpp URL is DaveNBusters the customer name is John Doe and his customer id is 39991. He is buying the item two hundred tickets package, which costs in total $200, the quantity of this order is 1. He is also buying two skittles family sized for $4.99 each.';
       this.dialog = false;
     }
   },
