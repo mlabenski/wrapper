b0b89f7b81d6180ebb81119576c8c7a19a489300
geebski <mlabenski123@gmail.com>
2023-05-29 22:01:55 +0000

    pt8

--- pages/generate-link/index.vue
+++ pages/generate-link/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col cols="12" sm="6" class="pb-3">
-      <v-card class="pa-4">
+      <v-card class="pa-4 pt-8">
         <div class="suggestions mb-2">
           <v-chip 
             v-for="(suggestion, index) in suggestions" 
